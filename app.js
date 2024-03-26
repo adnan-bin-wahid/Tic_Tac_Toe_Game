31a873eb48d593cd605667440a35d1803fef1ee9
adnan-bin-wahid <adnan.iit.du@gmail.com>
2024-03-26 12:38:27 +0600

    made the game without new and reset buttons

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset");
+let newBtn = document.querySelector("#newBtn");
+let msgContainer = document.querySelector(".msgContainer");
+let msg = document.querySelector("#msg");
 
 let player0 = true;
 
@@ -13,6 +16,10 @@ let winningPattern =[
     [0,4,8],
     [2,4,6]
 ];
+
+const resetGame =()=>{
+
+}
 
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
@@ -36,6 +43,16 @@ boxes.forEach((box)=>{
 //     console.log(boxes[pattern[0]],boxes[pattern[1]],boxes[pattern[2]]);
 // }
 
+const disabledBoxes = ()=>{
+    for(box of boxes){
+        box.disabled = true;
+    }
+}
+const showWinner=(winner)=>{
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disabledBoxes();
+}
 let checkWinner=()=>{
     for(pattern of winningPattern){
         // console.log(pattern[0],pattern[1],pattern[2]);
@@ -48,8 +65,10 @@ let checkWinner=()=>{
         if(position1 !=="" && position2 !=="" && position3 !==""){
             if(position1 === position2 && position2 === position3){
                 console.log("Winner");
+                showWinner(position1);
             }
         }
     }
 }
+
 
